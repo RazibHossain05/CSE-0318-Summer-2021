8cb80396a5210e736bf763b6a9ddbf0a3097884c
Md Razib Hossain <86564965+RazibHossain05@users.noreply.github.com>
2021-09-23 19:17:00 +0600

    Add files via upload

---
+++ Final Project Submission/code/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blood Bank Home</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="header">
+	<div class="logo"><h2> Blood Bank Management System </h2></div>
+	<div class="nav">
+		<div id="a"><a href="#">Home</a></div>
+		<div id="b"><a href="#">About As</a></div>
+			<div id="c"><a href="#">Contect Us</a></div>
+				<div id="d"><a href="#">Help</a></div>
+ </div>
+</div>
+<div class="banner"><h2 align="center" style="color: white"><br><br><br><br><br><br>Blood Bank</h2></div>
+<div class="container">
+	<br>
+	<h2 align="center" style="color: red">Doctors's Information</h2>
+       <br><br>
+      <center><table border="1px">
+	<tr>
+		<td width="200px" height="50px" style="color: red"><center><b>Doctor's name</b></center></td>
+		<td width="200px" height="50px" style="color: red"><center><b>Mobile no</b></center></td>
+		<td width="200px" height="50px" style="color: red"><center><b>Address</b></center></td>
+		<td width="200px" height="50px" style="color: red"><center><b>Spaclization</b></center></td>
+	</tr>
+	<tr>
+		<td width="200px" height="50px"><center>Md Razib</center></td>
+		<td width="200px" height="50px"><center>017486226..</center></td>
+		<td width="200px" height="50px"><center>H-12 Dhanmondi</center></td>
+		<td width="200px" height="50px"><center>Neurosurgeon</center></td>
+	</tr>
+</table></center>
+<div class="footer"><h2 align="center">Copyright@myprojecthd 2021</h2></div>
+</body>
+</html>
